8591521cd5a73011a938695666f1d3bdcb5ad992
RAMZ-66 <axelramz_@hotmail.com>
2021-02-10 11:05:47 -0700

    Creación del programa FizzBuzz

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = new int[100];
+
+            for (int i = 0; i < numeros.Length; i++) 
+            {
+                numeros[i] = i + 1;
+
+                if (((numeros[i] % 3) == 0) && ((numeros[i] % 5) == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if ((numeros[i] % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if ((numeros[i] % 5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+              
+                else 
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+                
+            }
+
+
         }
     }
 }
